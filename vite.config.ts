65314c4acfd5c92727378d21c47779795013f3a2
João Paulo Santana <paulo.jp806@gmail.com>
2026-06-01 21:03:20 -0300

    chore: configurar deploy no github pages com base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Hospital-de-Amor/',
   plugins: [react()],
   resolve: {
     alias: {
